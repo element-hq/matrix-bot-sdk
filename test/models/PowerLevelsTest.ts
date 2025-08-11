ee876f59b030cf22464e3927172c81cd446cbaf0
Will Hunt <will@half-shot.uk>
2025-08-11 16:22:27 +0100

    Add a helper to determine power levels for rooms (#64)
    * Add PowerLevel Manager
    
    * lint
    
    * Update src/models/PowerLevels.ts
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>
    
    * Update src/models/PowerLevels.ts
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>
    
    * Update src/models/PowerLevels.ts
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>
    
    * Update src/models/PowerLevels.ts
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>
    
    * Update src/models/PowerLevels.ts
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>
    
    * Update src/models/PowerLevels.ts
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>
    
    ---------
    
    Co-authored-by: Andrew Ferrazzutti <andrewf@element.io>

---
+++ test/models/PowerLevelsTest.ts
@@ -0,0 +1,180 @@
+import {
+    type PowerLevelsEventContent,
+    PLManager,
+    ROOM_ADMIN_PL,
+    ROOM_CREATOR_PL,
+} from "../../src";
+
+const PrimaryCreator = "@alice:bar";
+const ExtraCreators = Object.freeze(["@bob:bar", "@charlie:bar"]);
+
+type TestCandidate = [
+    {
+        creators: string[];
+        canAdjustPL: boolean;
+        creatorPL: number;
+    },
+    { room_version?: string, additional_creators?: readonly string[] },
+    PowerLevelsEventContent | undefined,
+];
+
+const StateSets: TestCandidate[] = [
+    // Without extra creators
+    [
+        { creators: [PrimaryCreator], canAdjustPL: true, creatorPL: 0 },
+        {},
+        undefined,
+    ],
+    [
+        { creators: [PrimaryCreator], canAdjustPL: true, creatorPL: 0 },
+        { room_version: "1" },
+        undefined,
+    ],
+    [
+        {
+            creators: [PrimaryCreator],
+            canAdjustPL: false,
+            creatorPL: ROOM_CREATOR_PL,
+        },
+        { room_version: "12" },
+        undefined,
+    ],
+    [
+        {
+            creators: [PrimaryCreator],
+            canAdjustPL: false,
+            creatorPL: ROOM_CREATOR_PL,
+        },
+        { room_version: "org.matrix.hydra.11" },
+        undefined,
+    ],
+
+    // With extra creators
+    [
+        { creators: [PrimaryCreator], canAdjustPL: true, creatorPL: 0 },
+        { additional_creators: ExtraCreators },
+        undefined,
+    ],
+    [
+        { creators: [PrimaryCreator], canAdjustPL: true, creatorPL: 0 },
+        { room_version: "1", additional_creators: ExtraCreators },
+        undefined,
+    ],
+    [
+        {
+            creators: [PrimaryCreator, ...ExtraCreators],
+            canAdjustPL: false,
+            creatorPL: ROOM_CREATOR_PL,
+        },
+        { room_version: "12", additional_creators: ExtraCreators },
+        undefined,
+    ],
+    [
+        {
+            creators: [PrimaryCreator, ...ExtraCreators],
+            canAdjustPL: false,
+            creatorPL: ROOM_CREATOR_PL,
+        },
+        {
+            room_version: "org.matrix.hydra.11",
+            additional_creators: ExtraCreators,
+        },
+        undefined,
+    ],
+
+    // With PLs
+    [
+        {
+            creators: [PrimaryCreator],
+            canAdjustPL: true,
+            creatorPL: ROOM_ADMIN_PL,
+        },
+        { additional_creators: ExtraCreators },
+        { users: { [PrimaryCreator]: ROOM_ADMIN_PL } },
+    ],
+    [
+        {
+            creators: [PrimaryCreator],
+            canAdjustPL: true,
+            creatorPL: ROOM_ADMIN_PL,
+        },
+        { room_version: "1", additional_creators: ExtraCreators },
+        { users: { [PrimaryCreator]: ROOM_ADMIN_PL } },
+    ],
+    [
+        {
+            creators: [PrimaryCreator, ...ExtraCreators],
+            canAdjustPL: false,
+            creatorPL: ROOM_CREATOR_PL,
+        },
+        { room_version: "12", additional_creators: ExtraCreators },
+        { users: { [PrimaryCreator]: ROOM_ADMIN_PL } },
+    ],
+    [
+        {
+            creators: [PrimaryCreator, ...ExtraCreators],
+            canAdjustPL: false,
+            creatorPL: ROOM_CREATOR_PL,
+        },
+        {
+            room_version: "org.matrix.hydra.11",
+            additional_creators: ExtraCreators,
+        },
+        { users: { [PrimaryCreator]: ROOM_ADMIN_PL } },
+    ],
+];
+
+describe("PLManager", function() {
+    it("should throw if the room is missing create state", function() {
+        expect(() => PLManager.createFromRoomState([])).toThrow(
+            "Could not find create event for room, cannot handle",
+        );
+    });
+
+    for (const [expected, createEventContent, plContent] of StateSets) {
+        const createEvent = {
+            type: "m.room.create",
+            state_key: "",
+            sender: PrimaryCreator,
+            content: createEventContent,
+        };
+        const powerLevelEvent = plContent && {
+            type: "m.room.power_levels",
+            state_key: "",
+            sender: PrimaryCreator,
+            content: plContent,
+        };
+
+        describe(`with room version ${createEvent.content.room_version} and extra creators=${createEvent.content.additional_creators}`, function() {
+            let plManager: PLManager;
+
+            beforeEach(function() {
+                plManager = PLManager.createFromRoomState([
+                    createEvent,
+                    ...(powerLevelEvent ? [powerLevelEvent] : []),
+                ]);
+            });
+
+            it(`Get correct set of creators with room version`, function() {
+                expect(plManager.getFirstCreator).toEqual(PrimaryCreator);
+                expect([...plManager.creators]).toEqual(
+                    expected.creators,
+                );
+            });
+
+            it(`Correctly calculates canAdjustUserPL`, function() {
+                for (const creator of plManager.creators) {
+                    expect(plManager.canAdjustUserPL(creator)).toEqual(
+                        expected.canAdjustPL,
+                    );
+                }
+            });
+
+            it(`Correctly calculates getUserPowerLevel`, function() {
+                expect(plManager.getUserPowerLevel(PrimaryCreator)).toEqual(
+                    expected.creatorPL,
+                );
+            });
+        });
+    }
+});
